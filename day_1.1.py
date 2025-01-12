2e21286c1ff2459f30d0b46bd43a818bee6b5c2f
w-ema <wema314@gmail.com>
2025-01-12 21:16:41 +0100

    first part of 1 day

---
+++ day_1.1.py
@@ -0,0 +1,35 @@
+with open(r"C:\Users\cp\Desktop\AoC-24\inputs\input_1.txt") as source:
+    inp = source.read()
+
+
+def sol(x):
+    left = list()
+    right = list()
+
+    for i in x.splitlines():
+        left.append(i.split("   ")[0])
+        right.append(i.split("   ")[1])
+
+    left.sort()
+    right.sort()
+
+    ans = 0
+    for i in range(len(left)):
+        ans += abs(int(left[i])-int(right[i]))
+    return ans
+
+
+test = '''3   4
+4   3
+2   5
+1   3
+3   9
+3   3'''
+
+
+def test_sol():
+    assert sol(test) == 11
+
+
+test_sol()
+print(sol(inp))
